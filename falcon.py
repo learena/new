3d1f6081275266630e97e0e8f92f68474f0e89e6
learena <67345676+learena@users.noreply.github.com>
2025-01-12 17:55:48 +0100

    Update falcon.py

--- falcon.py
+++ falcon.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from langchain_community.document_loaders import TextLoader
 from pypdf import PdfReader
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.memory import ConversationBufferWindowMemory
